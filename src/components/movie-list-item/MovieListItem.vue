aa087b18041c72ee569c67557af53fa019560ea8
ffozilbek <fozilbekfayzullayev05@gmail.com>
2025-03-28 22:25:40 +0500

    add  movie count span in movie item

--- src/components/movie-list-item/MovieListItem.vue
+++ src/components/movie-list-item/MovieListItem.vue
@@ -1,10 +1,13 @@
 <template>
     <li class="list-group-item d-flex align-items-center justify-content-between">
         Spiderman
-        <div class="btn-group">
-            <button class="btn btn-outline-dark"><i class="fa-solid fa-check"></i></button>
-            <button class="btn btn-outline-dark"><i class="fa-solid fa-video-slash"></i></button>
-            <button class="btn btn-outline-dark"><i class="fa-solid fa-eye"></i></button>
+        <div>
+            <span class="me-4">856</span>
+            <div class="btn-group">
+                <button class="btn btn-outline-dark"><i class="fa-solid fa-check"></i></button>
+                <button class="btn btn-outline-dark"><i class="fa-solid fa-video-slash"></i></button>
+                <button class="btn btn-outline-dark"><i class="fa-solid fa-eye"></i></button>
+            </div>
         </div>
     </li>
 </template>
